0901efb2ade77fc5272d6f20d7f97e539f2ba3ba
Endurance Aloja <alojaeddie@gmail.com>
2024-07-08 22:27:03 +0100

    move screens into own file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,53 +1,29 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageSourcePropType, ImageProps } from 'react-native'
 import React from 'react'
 import { StatusBar } from "expo-status-bar";
 import { Redirect, Tabs } from "expo-router";
-import {
-  Cog6ToothIcon,
-  PresentationChartLineIcon,
-  BuildingLibraryIcon,
-  DocumentPlusIcon,
-  Square3Stack3DIcon
-} from 'react-native-heroicons/outline';
+import screens from '@/constants/tabScreens';
+import somethig from "../"
 
-
-
+interface TabProps {
+  color: string
+  focused?: boolean
+  name?: string
+  icon: ImageSourcePropType | undefined
+}
+const TabIcon: React.FC<TabProps> = ({ color, focused, icon, name }) => {
+  return <View>
+    <Image source={icon} resizeMode='contain' style={styles.tabIcon} />
+  </View>
+}
 
 const TabsLayout = () => {
-
-  const screens = [
-    {
-      name: "Home",
-      title: "Home",
-      icon: <BuildingLibraryIcon height={30} width={30} color={"black"} />
-    },
-    {
-      name: "Categories",
-      title: "Categories",
-      icon: <Square3Stack3DIcon height={30} width={30} color={"black"} />
-    },
-    {
-      name: "Create",
-      title: "Create",
-      icon: <DocumentPlusIcon height={30} width={30} color={"black"} />
-    },
-    {
-      name: "Analytics",
-      title: "Analytics",
-      icon: <PresentationChartLineIcon height={30} width={30} color={"black"} />
-    },
-    {
-      name: "Settings",
-      title: "Settings",
-      icon: <Cog6ToothIcon height={30} width={30} color={"black"} />
-    },
-  ]
   return (
-    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }} >
-      {screens.map((screen) => <Tabs.Screen name={screen.name} options={{
-        title: screen.title, tabBarIcon: ({ color }) => <View style={styles.tabIcon}>{screen.icon}</View>
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false, }} >
+      {screens.map((screen, index) => <Tabs.Screen key={index} name={screen.name} options={{
+        title: screen.title, tabBarIcon: ({ color, focused }) => <TabIcon icon={screen.icon} />
       }} />)}
-    </Tabs>
+    </Tabs >
   )
 }
 
@@ -55,9 +31,9 @@ export default TabsLayout
 
 const styles = StyleSheet.create({
   tabIcon: {
-    flex: 1,
-    gap: 2,
-    alignItems: "center"
+    tintColor: "#000000",
+    width: 32,
 
   }
+
 })
---
+++ constants/tabScreens.tsx
@@ -0,0 +1,42 @@
+import {
+  Cog6ToothIcon,
+  PresentationChartLineIcon,
+  BuildingLibraryIcon,
+  DocumentPlusIcon,
+  Square3Stack3DIcon
+} from 'react-native-heroicons/outline';
+
+import icons from '../constants/icons';
+
+import { StyleSheet, View, Image } from 'react-native';
+import { SvgProps } from 'react-native-svg';
+
+const screens = [
+  {
+    name: "Home",
+    title: "Home",
+    icon: icons.home
+  },
+  {
+    name: "Categories",
+    title: "Categories",
+    icon: icons.categories
+  },
+  {
+    name: "Create",
+    title: "Create",
+
+  },
+  {
+    name: "Analytics",
+    title: "Analytics",
+    icon: icons.stats
+  },
+  {
+    name: "Settings",
+    title: "Settings",
+    icon: icons.settings
+  },
+]
+
+export default screens  
