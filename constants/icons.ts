951145e6170dda7a846dcb559487cdfe178f36a5
Endurance Aloja <alojaeddie@gmail.com>
2024-07-08 22:26:21 +0100

    change current icons

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "../assets/images/home-icon.png"
+import categories from "../assets/images/categories-icon.png"
+import settings from "../assets/images/settings-icon.png"
+import stats from "../assets/images/stats-icon.png"
+
+export default {
+  home,
+  categories,
+  settings,
+  stats
+}
