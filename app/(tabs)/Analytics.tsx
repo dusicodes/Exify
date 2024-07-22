be35e1ead00639788fff6b8313ab2e0a96ad7ddb
Endurance Aloja <alojaeddie@gmail.com>
2024-07-22 17:21:45 +0100

    Add onboarding images

--- app/(tabs)/Analytics.tsx
+++ app/(tabs)/Analytics.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Analytics = () => {
   return (
     <View>
       <Text>Analytics</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Analytics
+export default Analytics;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
